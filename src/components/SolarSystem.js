3a7a20d2246df60b49f3f4a7b894af9a3fa63506
Will Young <will.young93@gmail.com>
2019-03-11 15:24:33 -0500

    sun centered

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
-import Sun from './Sun';
-import Earth from './Earth';
-import Venus from './Venus';
-import Mercury from './Mercury';
-import Mars from './Mars';
+import React, { Component } from "react";
+import Sun from "./Sun";
+import Earth from "./Earth";
+import Venus from "./Venus";
+import Mercury from "./Mercury";
+import Mars from "./Mars";
 
 export class SolarSystem extends Component {
-	render() {
-		return (
-			<div className="outer-div">
-				<div className="planet-div-container">
-					<div id="expo" className="earth-div">
-						<Earth />
-					</div>
+  render() {
+    return (
+      <div className="outer-div">
+        <div className="planet-div-container">
+          <div id="expo" className="earth-div">
+            <Earth />
+          </div>
 
-					<div className="venus-div">
-						<Venus />
-					</div>
+          <div className="venus-div">
+            <Venus />
+          </div>
 
-					<div className="mercury-div">
-						<Mercury />
-					</div>
+          <div className="mercury-div">
+            <Mercury />
+          </div>
 
-					<div className="mars-div">
-						<Mars />
-					</div>
-					<div className="sun-div-container">
-						<div className="sun-div">
-							<Sun />
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+          <div className="mars-div">
+            <Mars />
+          </div>
+
+          <div className="sun-div">
+            <Sun />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SolarSystem;
