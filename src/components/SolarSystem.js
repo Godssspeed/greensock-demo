c1738a151c7595a1b64707fac340cddef3119d98
Caleb Smith <calebfrances@msn.com>
2019-03-11 11:29:30 -0500

    added div containers to planet and sun div

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,32 +1,36 @@
-import React, { Component } from "react";
-import Sun from "./Sun";
-import Earth from "./Earth";
-import Venus from "./Venus";
-import Mercury from "./Mercury";
-import Mars from "./Mars";
+import React, { Component } from 'react';
+import Sun from './Sun';
+import Earth from './Earth';
+import Venus from './Venus';
+import Mercury from './Mercury';
+import Mars from './Mars';
 
 export class SolarSystem extends Component {
-  render() {
-    return (
-      <div className="outer-div">
-        <div className="sun-div">
-          <Sun />
-        </div>
-        <div className="earth-div">
-          <Earth />
-        </div>
-        <div className="venus-div">
-          <Venus />
-        </div>
-        <div className="mercury-div">
-          <Mercury />
-        </div>
-        <div className="mars-div">
-          <Mars />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="outer-div">
+				<div className="sun-div-container">
+					<div className="sun-div">
+						<Sun />
+					</div>
+				</div>
+				<div className="planet-div-container">
+					<div className="earth-div">
+						<Earth />
+					</div>
+					<div className="venus-div">
+						<Venus />
+					</div>
+					<div className="mercury-div">
+						<Mercury />
+					</div>
+					<div className="mars-div">
+						<Mars />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default SolarSystem;
