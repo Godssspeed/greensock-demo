0ccc1812119a3d8fbfb971994878765af23763c9
Will Young <will.young93@gmail.com>
2019-03-11 14:55:42 -0500

    dont know what happened

--- src/components/Mercury.js
+++ src/components/Mercury.js
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
+import { TweenLite, TweenMax, ExpoScaleEase } from "gsap";
 import "./Mercury.css";
 
 export class Mercury extends Component {
+  constructor() {
+    super();
+    this.myElement = null;
+    this.myTween = null;
+    this.state = {
+      showMercury: false
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.state.showMercury) {
+      this.myTween = TweenMax.fromTo(
+        this.myElement,
+        3,
+        { scale: 1 },
+        { scale: 3, ease: ExpoScaleEase.config(20, 10), repeat: 0 }
+      );
+    } else {
+      this.myTween = TweenMax.fromTo(
+        this.myElement,
+        3,
+        { scale: 3 },
+        { scale: 1, ease: ExpoScaleEase.config(20, 10), repeat: 0 }
+      );
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ showMercury: !this.state.showMercury });
+  };
   render() {
     return (
-      <div className="mercury">
-        <img src="https://s3.us-east-2.amazonaws.com/solarsystem/mercury.png" />
+      <div className="mercury" ref={div => (this.myElement = div)}>
+        <img
+          src="https://s3.us-east-2.amazonaws.com/solarsystem/mercury.png"
+          onClick={this.handleClick}
+        />
       </div>
     );
   }
--- src/components/Venus.js
+++ src/components/Venus.js
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
+import { TweenLite, TweenMax, ExpoScaleEase } from "gsap";
 import "./Venus.css";
 
 export class Venus extends Component {
+  constructor() {
+    super();
+    this.myElement = null;
+    this.myTween = null;
+    this.state = {
+      showVenus: false
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.state.showVenus) {
+      this.myTween = TweenMax.fromTo(
+        this.myElement,
+        3,
+        { scale: 1 },
+        { scale: 3, ease: ExpoScaleEase.config(20, 10), repeat: 0 }
+      );
+    } else {
+      this.myTween = TweenMax.fromTo(
+        this.myElement,
+        3,
+        { scale: 3 },
+        { scale: 1, ease: ExpoScaleEase.config(20, 10), repeat: 0 }
+      );
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ showVenus: !this.state.showVenus });
+  };
   render() {
     return (
-      <div className="venus">
-        <img src="https://s3.us-east-2.amazonaws.com/solarsystem/venus.png" />
+      <div className="venus" ref={div => (this.myElement = div)}>
+        <img
+          src="https://s3.us-east-2.amazonaws.com/solarsystem/venus.png"
+          onClick={this.handleClick}
+        />
       </div>
     );
   }
