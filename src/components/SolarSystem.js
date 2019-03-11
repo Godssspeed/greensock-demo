50519564b92ddc44134ca4034de30e694e2ae676
Will Young <will.young93@gmail.com>
2019-03-11 11:05:20 -0500

    slight css

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import Sun from './Sun';
-import Earth from './Earth';
-import Venus from './Venus';
-import Mercury from './Mercury';
-import Mars from './Mars';
+import React, { Component } from "react";
+import Sun from "./Sun";
+import Earth from "./Earth";
+import Venus from "./Venus";
+import Mercury from "./Mercury";
+import Mars from "./Mars";
 
 export class SolarSystem extends Component {
-	render() {
-		return (
-			<div>
-				<div className="sun-div">
-					<Sun />
-				</div>
-				<div className="earth-div">
-					<Earth />
-				</div>
-				<div className="venus-div">
-					<Venus />
-				</div>
-				<div className="mercury-div">
-					<Mercury />
-				</div>
-				<div className="mars-div">
-					<Mars />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="outer-div">
+        <div className="sun-div">
+          <Sun />
+        </div>
+        <div className="earth-div">
+          <Earth />
+        </div>
+        <div className="venus-div">
+          <Venus />
+        </div>
+        <div className="mercury-div">
+          <Mercury />
+        </div>
+        <div className="mars-div">
+          <Mars />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SolarSystem;
